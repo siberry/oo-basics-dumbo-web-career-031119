115155838fdbf566fcffb3e671e4ecfb55719400
Serena Berry <siberry226@gmail.com>
2019-03-13 19:35:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,10 +3,14 @@
 class Shoe
   
   attr_reader :brand 
-  attr_accessor :color, :size, :material
+  attr_accessor :color, :size, :material, :condition
   
   def initialize(brand)
     @brand = brand
+  end
+  
+  def cobble
+    shoe.condition = "new"
   end
   
 end
